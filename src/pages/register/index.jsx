58a2a86aba4e67e5993f6b8a3f407cb5712b02a6
ZiHangQin <1420014281@qq.com>
2023-05-19 23:42:22 +0800

    完善注册请求

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -2,6 +2,7 @@
 import { Form, Button, Input } from "antd";
 import { Link } from "react-router-dom";
 import './register.css'
+import axios from "axios";
 
 
 const Inner = () => {
@@ -10,7 +11,7 @@ const Inner = () => {
             <div className="inner">
                 <span>i</span>
                 <h1>注册</h1>
-                <p>这是个注册功能，请正确填写</p>
+                <p>这是个注册功能，请正确填写。携带*号的是必填项，不带*号的可选填</p>
             </div>
         </div>
     )
@@ -28,6 +29,26 @@ const Back = () => {
 function Register() {
     const onFinish = (values) => {
         console.log(values)
+        if (!values.userName || !values.password || !values.email) {
+            alert("账号密码不能为空");
+            return;
+        } else {
+            axios.post('http://127.0.0.1:8080/api/account/register', {
+                username: values.userName,
+                password: values.password,
+                phone: values.phone,
+                email: values.email
+            })
+                .then(response => {
+                    console.log("注册成功")
+                    alert("注册成功");
+                    window.location.href = "/login"
+                })
+                .catch(error => {
+                    console.log(error.response.data);
+                    alert(error.response.data.Msg)
+                })
+        }
 
     }
 
@@ -54,14 +75,14 @@ function Register() {
                 >
                     <div>
                         <h3>账号注册</h3>
-                        <label>用户名：</label>
+                        <label>*用户名：</label>
                         <Input
                             type="text" name="userName" placeholder="请输入用户名" id="userName" />
                     </div>
                 </Form.Item>
 
                 <Form.Item
-                    name="emil"
+                    name="email"
                     rules={[
                         {
                             required: true,
@@ -70,9 +91,19 @@ function Register() {
                     ]}
                 >
                     <div>
-                        <label>邮箱：</label>
+                        <label>*邮箱：</label>
                         <Input
-                            type="text" name="emil" placeholder="请输入邮箱" id="emil" />
+                            type="text" name="email" placeholder="请输入邮箱" id="email" />
+                    </div>
+                </Form.Item>
+
+                <Form.Item
+                    name="phone"
+                >
+                    <div>
+                        <label>手机号：</label>
+                        <Input
+                            type="text" name="phone" placeholder="请输入手机号" id="phone" />
                     </div>
                 </Form.Item>
 
@@ -86,7 +117,7 @@ function Register() {
                     ]}
                 >
                     <div>
-                        <label>密码：</label>
+                        <label>*密码：</label>
                         <Input
                             type="password" name="password" placeholder="密码" id="password" />
                     </div>
@@ -102,7 +133,7 @@ function Register() {
                     <Button type="primary" className="buttonLogin" htmlType="submit">
                         注册账号
                     </Button>
-                    <br/>
+                    <br />
                     <Button type="link" className="buttonRegister">
                         <Link to="/login">
                             已有账号去登录
