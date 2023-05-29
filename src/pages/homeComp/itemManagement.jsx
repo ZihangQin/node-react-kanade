ebff7612573510553051cdaa911db7f0ce37a084
ZiHangQin <1420014281@qq.com>
2023-05-29 21:49:06 +0800

    新增：新增试题管理页面中的批量删除和多选框

--- src/pages/homeComp/itemManagement.jsx
+++ src/pages/homeComp/itemManagement.jsx
@@ -11,7 +11,8 @@ export default class DynamicTable extends React.Component {
             TitlePages: 0,
             page: 1, // 初始化当前页数为1
             loading: false, // 添加一个loading状态用于展示数据加载中的提示
-            error: null // 添加一个error状态用于展示请求数据时出现的错误信息
+            error: null, // 添加一个error状态用于展示请求数据时出现的错误信息
+            checked: {} // 存储哪些复选框被选中了
         };
     }
 
@@ -19,10 +20,10 @@ export default class DynamicTable extends React.Component {
         this.setState({ loading: true }); // 开始请求数据前将loading状态设置为true
 
         try {
-            const response = await axios.get('http://127.0.0.1:8080/api/browse/testList?page='+this.state.page);
+            const response = await axios.get('http://127.0.0.1:8080/api/browse/testList?page=' + this.state.page);
             const { TestLists, TitlePages } = response.data.Data;
             this.setState({ TestLists, TitlePages, loading: false }); // 请求数据成功后将loading状态设置为false
-        } catch(err) {
+        } catch (err) {
             console.log('请求数据出错', err);
             this.setState({ error: '请求数据出错', loading: false }); // 请求数据出现错误时将error状态设置为请求数据出错的信息并将loading状态设置为false
         }
@@ -32,21 +33,59 @@ export default class DynamicTable extends React.Component {
         this.setState({ loading: true }); // 开始请求数据前将loading状态设置为true
 
         try {
-            const response = await axios.get('http://127.0.0.1:8080/api/browse/testList?page='+page);
+            const response = await axios.get('http://127.0.0.1:8080/api/browse/testList?page=' + page);
             const { TestLists, TitlePages } = response.data.Data;
             this.setState({ TestLists, TitlePages, page, loading: false, error: null }); // 更新state中的data、totalPage和page属性并将loading状态设置为false，清除error状态
-        } catch(err) {
+        } catch (err) {
             console.log('请求数据出错', err);
             this.setState({ error: '请求数据出错', loading: false }); // 请求数据出现错误时将error状态设置为请求数据出错的信息并将loading状态设置为false
         }
     }
+
+    handleSelectAll = (e) => {
+        const checkboxes = document.querySelectorAll('input[type="checkbox"][name="myCheckbox"]'); // 获取所有复选框的DOM节点列表
+        const checked = {};
+        checkboxes.forEach((checkbox) => {
+            checkbox.checked = e.target.checked; // 遍历所有DOM节点并将其checked属性设置为全选复选框的选中状态
+            const { id } = checkbox;
+            if(checkbox.checked){
+                checked[checkbox.getAttribute('id')] =  id// 将每个复选框的选中状态存储到checked对象中
+            }else{
+               delete checked[checkbox.getAttribute('id')] // 将每个复选框的选中状态存储到checked对象中
+            }
+        });
+        this.setState({ checked }); // 设置state中的checked属性
+    }
+
+    // 将复选框的选中状态存储到state中的checked属性中
+    handleCheckboxChange = (e) => {
+        const { id } = e.target;
+        const { checked: oldChecked } = this.state;
+        let newChecked;
+        if (oldChecked[id]) {
+            // 如果当前复选框之前被选中，则取消选中并从checked对象中删除该条数据
+            delete oldChecked[id];
+            e.target.checked = "";
+            newChecked = { ...oldChecked };
+        } else {
+            // 否则，将当前复选框的选中状态存储到checked对象中
+            newChecked = { ...oldChecked, [id]: id };
+        }
+        this.setState({ checked: newChecked });
+    }
+
+    getCheckTrue = (e) => {
+        console.log(this.state.checked)
+        //此处后端请求批量删除
+    }
+
 
     render() {
         const { TestLists, TitlePages, page, loading, error } = this.state;
 
         // 如果发生了错误，则展示错误信息
         if (error) {
-          return <div>{error}</div>;
+            return <div>{error}</div>;
         }
 
         return (
@@ -67,71 +106,76 @@ export default class DynamicTable extends React.Component {
                         <div>数据加载中...</div> // 如果loading状态为true，则展示数据加载中的提示信息
                     ) : (
                         <>
-                            {TestLists.length > 0 ?( // 如果有数据则展示表格
+                            {TestLists.length > 0 ? ( // 如果有数据则展示表格
                                 <table>
-                                <thead>
-                                    <tr>
-                                        <th>序号</th>
-                                        <th>题目</th>
-                                        <th>班级</th>
-                                        <th>分值</th>
-                                        <th>题目类型</th>
-                                        <th>难度系数</th>
-                                        <th>出题人</th>
-                                        <th>答案</th>
-                                        <th>时间</th>
-                                        <th>操作</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {TestLists.map((item) => (
-                                        <tr key={item.id}>
-                                            <td>{item.id}</td>
-                                            <td>{item.title}</td>
-                                            <td>{item.class}</td>
-                                            <td>{item.score}</td>
-                                            <td>{item.title_type}</td>
-                                            <td>{item.difficulty}</td>
-                                            <td>{item.questionsSetter}</td>
-                                            <td>{item.answer}</td>
-                                            <td>{item.updateAt}</td>
-                                            <td>
-                                                <button type="submit">修改</button>
-                                                &nbsp; &nbsp; &nbsp;
-                                                <button type="submit">删除</button>
-                                            </td>
+                                    <thead>
+                                        <tr>
+                                            <th>
+                                                <input type="checkbox" id="myCheckbox" name="myCheckbox" value="1" onChange={this.handleSelectAll} />
+                                            </th>
+                                            <th>序号</th>
+                                            <th>题目</th>
+                                            <th>班级</th>
+                                            <th>分值</th>
+                                            <th>题目类型</th>
+                                            <th>难度系数</th>
+                                            <th>出题人</th>
+                                            <th>答案</th>
+                                            <th>时间</th>
+                                            <th>操作</th>
                                         </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                            ):(
-                                <>
-                                <table>
-                                <thead>
-                                    <tr>
-                                        <th>序号</th>
-                                        <th>题目</th>
-                                        <th>班级</th>
-                                        <th>分值</th>
-                                        <th>题目类型</th>
-                                        <th>难度系数</th>
-                                        <th>出题人</th>
-                                        <th>答案</th>
-                                        <th>时间</th>
-                                        <th>操作</th>
-
-                                    </tr>
-                                </thead>
+                                    </thead>
+                                    <tbody>
+                                        {TestLists.map((item) => (
+                                            <tr key={item.id}>
+                                                <td><input type="checkbox" id={item.id} name='myCheckbox' value="1" onChange={this.handleCheckboxChange} /></td>
+                                                <td>{item.id}</td>
+                                                <td>{item.title}</td>
+                                                <td>{item.class}</td>
+                                                <td>{item.score}</td>
+                                                <td>{item.title_type}</td>
+                                                <td>{item.difficulty}</td>
+                                                <td>{item.questionsSetter}</td>
+                                                <td>{item.answer}</td>
+                                                <td>{item.updateAt}</td>
+                                                <td>
+                                                    <button type="submit">修改</button>
+                                                    &nbsp; &nbsp; &nbsp;
+                                                    <button type="submit">删除</button>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
                                 </table>
-                                <div>暂无数据</div>
+                            ) : (
+                                <>
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th><input type="checkbox" id="myCheckbox" name="myCheckbox" value="1" /></th>
+                                                <th>序号</th>
+                                                <th>题目</th>
+                                                <th>班级</th>
+                                                <th>分值</th>
+                                                <th>题目类型</th>
+                                                <th>难度系数</th>
+                                                <th>出题人</th>
+                                                <th>答案</th>
+                                                <th>时间</th>
+                                                <th>操作</th>
+
+                                            </tr>
+                                        </thead>
+                                    </table>
+                                    <div>暂无数据</div>
                                 </>
                             )}
                         </>
                     )}
                 </div>
                 <div className="paging_button">
-                    <div><span>批量删除</span></div>
-                    <Pagination defaultCurrent={1} current={page} total={TitlePages*10} onChange={this.handleChangePage} />,
+                    <div onClick={this.getCheckTrue}><span>批量删除</span></div>
+                    <Pagination defaultCurrent={1} current={page} total={TitlePages * 10} onChange={this.handleChangePage} />,
                 </div>
             </>
         )
